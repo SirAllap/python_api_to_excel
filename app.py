737820bf9f7137e512af120d962124ae75540143
David PR <serallapdivad@gmail.com>
2024-01-18 16:27:02 +0000

    flask api correctly returning the fake data.json file data

--- app.py
+++ app.py
@@ -10,9 +10,13 @@ def get_json_data():
         data = json.load(json_file)
     return data
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def root():
-    return "I'm the root of the API"
+    return "I'm the root of the API, to check prodcust go to http://127.0.0.1:5001/products"
+
+@app.route('/products')
+def get_products():
+    return get_json_data()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
